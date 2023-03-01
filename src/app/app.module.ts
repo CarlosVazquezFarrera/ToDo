a52fd857ca1b46610fb63b0e21ffa3609c2d2e8d
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2023-03-01 14:16:38 -0500

    Addding menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MaterialModule } from './material.module';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { ToDoModule } from './components/todo/to-do.module';
+import { MenuComponent } from './shared/menu/menu.component';
 
 
 @NgModule({
@@ -17,6 +18,7 @@ import { ToDoModule } from './components/todo/to-do.module';
     HeaderComponent,
     FooterComponent,
     ActivitiesComponent,
+    MenuComponent,
   ],
   imports: [
     BrowserModule,
