3a45063534f84415ffa209d5bcd36089d4360d11
Carlos Vazquez <carlos.vazquez.unach@gmail.com>
2023-03-01 13:51:29 -0500

    New config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material.module';
-import { HeaderComponent } from './components/header/header.component';
 import { ActivitiesComponent } from './pages/activities/activities.component';
-import { ToDoModule } from './todo/to-do.module';
-import { FooterComponent } from './components/footer/footer.component';
+import { MaterialModule } from './material.module';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { ToDoModule } from './components/todo/to-do.module';
 
 
 @NgModule({
--- src/app/components/todo/to-do.module.ts
+++ src/app/components/todo/to-do.module.ts
@@ -3,10 +3,8 @@ import { CommonModule } from '@angular/common';
 import { AddComponent } from './add/add.component';
 import { ItemComponent } from './item/item.component';
 import { ListComponent } from './list/list.component';
-import { MaterialModule } from '../material.module';
 import { InfoComponent } from './info/info.component';
-
-
+import { MaterialModule } from 'src/app/material.module';
 
 @NgModule({
   declarations: [
